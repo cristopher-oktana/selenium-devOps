42c76a871d0db9cc81007197d519c23ccd67be29
cristopher-oktana <cristopher.cardenas@oktana.com>
2022-05-09 20:02:50 -0500

    readme

---
+++ README.txt
@@ -0,0 +1,6 @@
+Install Java 18 - https://www.oracle.com/java/technologies/downloads/
+Install IntelliJ Community - https://www.jetbrains.com/idea/download/#section=mac
+Install homebrew in Mac (admin) - https://brew.sh/
+brew install Maven (admin) in terminal
+    - su usuarioAdmin
+Download chromedriver according to the current version from the Chrome Browser - https://chromedriver.chromium.org/downloads
--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -8,6 +8,7 @@ public class BaseTest {
     private WebDriver driver;
     private String username="//input[@id='username']";
     private String password="//input[@id='password']";
+    private String BUTTON_LOGIN="//input[@id='Login']";
 
     public void setUp(){
         System.setProperty("webdriver.chrome.driver", "resources/chromedriver");
@@ -22,6 +23,7 @@ public class BaseTest {
         driver.get("https://teamorg2-dev-ed.my.salesforce.com/");
         driver.findElement(By.xpath(username)).sendKeys("cristopher.cardenas.blue.sky@oktana.com");
         driver.findElement(By.xpath(password)).sendKeys("Oktana.2022!");
+        driver.findElement(By.xpath(BUTTON_LOGIN)).click();
     }
 
     public static void main(String args[]){
