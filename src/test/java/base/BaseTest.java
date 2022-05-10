47d84fe801c1167ee649ae7a29b6f6d3dfa69768
cristopher-oktana <cristopher.cardenas@oktana.com>
2022-05-09 19:56:21 -0500

    first

---
+++ src/test/java/base/BaseTest.java
@@ -0,0 +1,31 @@
+package base;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BaseTest {
+    private WebDriver driver;
+    private String username="//input[@id='username']";
+    private String password="//input[@id='password']";
+
+    public void setUp(){
+        System.setProperty("webdriver.chrome.driver", "resources/chromedriver");
+        driver = new ChromeDriver();
+        driver.get("https://the-internet.herokuapp.com/");
+        System.out.println(driver.getTitle());
+    }
+
+    public void LoginSalesforce(){
+        System.setProperty("webdriver.chrome.driver", "resources/chromedriver");
+        driver = new ChromeDriver();
+        driver.get("https://teamorg2-dev-ed.my.salesforce.com/");
+        driver.findElement(By.xpath(username)).sendKeys("cristopher.cardenas.blue.sky@oktana.com");
+        driver.findElement(By.xpath(password)).sendKeys("Oktana.2022!");
+    }
+
+    public static void main(String args[]){
+        BaseTest test = new BaseTest();
+        test.LoginSalesforce();
+    }
+}
